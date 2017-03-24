3e76c0c7c5a6b0a09da6ad3fbc10d657ff0cfcee
kai <0ultrakai0@gmail.com>
2017-03-24 15:06:30 +0200

    Initial Commit

---
+++ Kai_Project/Source/Kai_Project/Kai_Project.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Kai_Project.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Kai_Project, "Kai_Project" );
---
+++ Kai_Project/Source/Kai_Project/Kai_ProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Kai_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KAI_PROJECT_API AKai_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
